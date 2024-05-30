20c3cdadc0b34511b86866391b3b311cf66040c0
hjopel <70332133+hjopel@users.noreply.github.com>
2024-05-30 14:11:24 +0200

    chore: remove unused metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,10 +37,6 @@ export const metadata: Metadata = {
     title: 'Jan Hoppel',
     card: 'summary_large_image'
   }
-  // verification: {
-  //   google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-  //   yandex: '14d2e73487fa6c71'
-  // }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
